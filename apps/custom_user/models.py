ce2c660feb19f3073dcc6307fe4b2add3fb1f91c
santosh <kakdesantosh993@gmail.com>
2024-12-18 11:15:15 +0530

    user registration & login

--- apps/custom_user/models.py
+++ apps/custom_user/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    phone_number = models.CharField(max_length=20, unique=True)
-    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True,blank=True, null=True)
+    email = models.EmailField(max_length=100, unique=True,blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', default='profile_pictures/default_profile.jpg',blank=True, null=True)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=100, blank=True)
+    bio = models.TextField(max_length=500,blank=True, null=True)
+    location = models.CharField(max_length=100,blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
 
 
